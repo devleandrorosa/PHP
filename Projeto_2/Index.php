aecb920b6ebe81de8e4bfe0c2b79d90aa9611783
Leandro Rosa <leandro.souza.f.lds@gmail.com>
2023-05-23 21:15:22 -0300

    atualização

--- Projeto_2/Index.php
+++ Projeto_2/Index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>PHP - Primeiro projeto</title>
+    <title>Projeto_2</title>
 </head>
 <body>
-    <h2>FORMULÁRIO</h2>
+    <h2>SOMA</h2>
     <?php
-        $n = 22; 
-        $no = "Gustavo";
-        echo "$no tem $n anos.";
+        $n1 = 2;
+        $n2 = 3;
+        $s = $n1 + $n2;
+        echo "A some entre $n1 e $n2 é $s";
     ?>
 </body>
 </html>
